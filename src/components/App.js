1bd8cced1c25623b9e3b54e989c8362a9fb8f6b9
Constantin Unterkofler <constantinu93@gmail.com>
2021-05-23 16:45:57 +0200

    feat: set up Redux Store

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HomeView from './HomeView';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <HomeView />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './components/App';
+import rootReducer from './reducers';
+import middleware from './middleware';
+
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

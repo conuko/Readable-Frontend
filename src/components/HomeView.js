e3e4f7474232b909d5fc5c44e0f7c24c70a8a3fa
Constantin <constantinu93@gmail.com>
2021-06-17 17:26:29 +0200

    add categories to HomeView; start building Posts component

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -7,22 +7,32 @@ order by voteScore and order by timestamp
 
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Post from './Post';
 
 function HomeView() {
   const allPosts = useSelector((state) => Object.values(state.posts));
+  const allCategories = useSelector((state) => Object.values(state.categories));
   return (
     <div>
-      <h2>This is the Home View</h2>
-      <ul className="dashboard-list">
+      <h1>Home View</h1>
+      <h2>Categories</h2>
+      <div className="categories-container">
+        <ul className="categories-list">
+          {allCategories.map((category) => (
+            <li key={category.name}>
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <h2>{`${allPosts.length} Posts`}</h2>
+      <ul className="posts-list">
         {allPosts.map((post) => (
           <li key={post.id}>
-            <div>
-              POST ID:
-              {` ${post.id}`}
-            </div>
+            <Post id={post.id} />
           </li>
         ))}
-
       </ul>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,13 +3,19 @@ The Posts at HomeView page.
 */
 
 import React from 'react';
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-function Post() {
+function Post({ id }) {
+  const posts = useSelector((state) => Object.values(state.posts));
+  const post = posts[id];
   return (
-    <div>
-      <p>This is the post shown on the HomeView Page</p>
-    </div>
+    <div className="post" />
   );
 }
+
+Post.propTypes = {
+  id: PropTypes.string.isRequired,
+};
 
 export default Post;

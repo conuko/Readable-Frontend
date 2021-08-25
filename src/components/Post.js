94d97fb4a1bc34d546b9592b9f586c58a69efb3c
Constantin Unterkofler <constantinu93@gmail.com>
2021-08-25 18:16:02 +0200

    style: start styling Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -35,55 +35,70 @@ function Post({ post }) {
     return <Redirect to="/" />;
   }
   return (
-    <div className="w-full max-w-xs m-auto">
+    <div className="flex-none md:flex-1 w-full max-w-xs m-auto">
       <div className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
-        {}
-        Score:
-        {' '}
-        {postScore}
-        {' '}
-        <br />
-        <button className="upvote-button" type="button" onClick={handleClickOnUpVoteButton}>
-          Upvote
-        </button>
-        <br />
-        <button className="downvote-button" type="button" onClick={handleClickOnDownVoteButton}>
-          Downvote
-        </button>
-        <br />
-        Author:
-        {' '}
-        {postAuthor}
-        {' '}
-        <br />
-        Date:
-        {' '}
-        {postDate}
-        {' '}
-        <br />
-        <Link to={`/post/${post.id}`}>
-          {postTitle}
-        </Link>
-        {' '}
-        <br />
-        <Link to={`/category/${post.category}`}>
-          <button type="button">
-            {post.category}
+        <div className="text-black-700 text-md mb-2">
+          <Link to={`/post/${post.id}`} className="block text-black-700 text-md font-bold mb-2">
+            {postTitle}
+          </Link>
+          <Link to={`/category/${post.category}`} className="block text-gray-700 text-md font-bold mb-2">
+            <button type="button">
+              {post.category}
+            </button>
+          </Link>
+          <p className="block mb-2 mt-2">
+            Posted by
+            {' '}
+            {postAuthor}
+            {' '}
+          </p>
+          <p className="flex flex-row block mb-2 mt-2">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
+            </svg>
+            {' '}
+            {postDate}
+          </p>
+          {' '}
+          <p>
+            {' '}
+            {' '}
+            {`${post.commentCount} comments`}
+          </p>
+        </div>
+        <div className="score-area">
+          <p>
+            Score:
+            {' '}
+            {postScore}
+            {' '}
+          </p>
+          <div className="score-buttons">
+            <button className="upvote-button" type="button" onClick={handleClickOnUpVoteButton}>
+              Upvote
+            </button>
+            <button className="downvote-button" type="button" onClick={handleClickOnDownVoteButton}>
+              Downvote
+            </button>
+          </div>
+        </div>
+        <div className="flex gap-6 justify-center">
+          <Link to={`/post/edit/${post.id}`}>
+            <button
+              className="font-bold bg-lime-300 text-black uppercase text-sm border border-black shadow-offset-black hover:bg-lime-500 py-2 px-4 focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Edit
+            </button>
+          </Link>
+          <button
+            className="font-bold bg-red-400 text-black uppercase text-sm border border-black shadow-offset-black hover:bg-red-600 py-2 px-4 focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={handleClickOnDeleteButton}
+          >
+            Delete
           </button>
-        </Link>
-        <br />
-        {' '}
-        {`${post.commentCount} comments`}
-        <br />
-        <button className="delete-button" type="button" onClick={handleClickOnDeleteButton}>
-          Delete
-        </button>
-        <Link to={`/post/edit/${post.id}`}>
-          <button className="edit-button" type="button">
-            Edit
-          </button>
-        </Link>
-        <br />
+        </div>
       </div>
     </div>
   );

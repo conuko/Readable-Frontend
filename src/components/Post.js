d6f3c96f771629f4f883e0bdd0ebb8a33b6e5dd3
Constantin <constantinu93@gmail.com>
2021-09-13 20:40:25 +0200

    feat: code refactoring

--- src/components/Post.js
+++ src/components/Post.js
@@ -46,13 +46,13 @@ function Post({ post }) {
               {post.category}
             </button>
           </Link>
-          <p className="flex flex-row font-bold block mb-2 mt-2">
+          <p className="flex flex-row font-bold mb-2 mt-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd" />
             </svg>
             {postAuthor}
           </p>
-          <p className="flex flex-row text-sm block mb-2 mt-2">
+          <p className="flex flex-row text-sm mb-2 mt-2">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
             </svg>

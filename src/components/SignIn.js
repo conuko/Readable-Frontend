ae9f3c51dd45ec89aded34ddecfecaa709fb1a35
Constantin <constantinu93@gmail.com>
2021-09-29 18:22:48 +0200

    finish structure of SignIn component

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,37 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
 
 function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setEmail('');
+    setPassword('');
+  };
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    if (name === 'password') {
+      setPassword(value);
+    } else {
+      setEmail(value);
+    }
+  };
+
   return (
-    <div className="sign-in-component">SIGN IN COMPONENT</div>
+    <div className="sign-in-component">
+      <h2>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="email" value={email} onChange={handleChange} required />
+        <label>Email</label>
+        <input name="password" type="password" value={password} onChange={handleChange} required />
+        <label>Password</label>
+        <input type="submit" value="Submit Form" />
+      </form>
+    </div>
   );
 }
 
--- src/components/SignInView.js
+++ src/components/SignInView.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import SignIn from './SignIn';
 
 function SignInView() {
   return (
     <div className="sign-in-and-sign-out">
-      SIGN IN
+      <SignIn />
     </div>
   );
 }
